887b670f5f7e4b5758d35858eafd1db264314233
Abhi Omar <abhiomar784@gmail.com>
2025-11-30 00:06:37 +0530

    Create Labo1_q2.c

---
+++ Lab01/Labo1_q2.c
@@ -0,0 +1,6 @@
+//Q2:Write a C Program to print the address in multiple lines (new line). At least 4 lines, use \n and only one "printf" function.
+#include <stdio.h>
+int main() {
+    printf("Mo.Tarin\nPhaphund\nAuraiya,UP\nindia");
+    return 0;
+}
